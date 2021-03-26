fdc4e452f610ad56ec0809206564fab216e48c6e
Woon <Woon@DESKTOP-K6AB7U8>
2021-03-26 17:17:36 +0900

    branch marge를 위한 commit

--- Baccarat/src/com/baccarat/main/BaccaratMain.java
+++ Baccarat/src/com/baccarat/main/BaccaratMain.java
@@ -3,6 +3,11 @@ package com.baccarat.main;
 public class BaccaratMain {
 
 	public static void main(String[] args) {
+		
+	}
+	
+	private void mainTemp() {
+		// TODO Auto-generated method stub
 		BaccaratGame game = new BaccaratGame();
 		game.play();
 	}
